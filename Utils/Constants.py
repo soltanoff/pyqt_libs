27d9d07ced5eb3d4d251b3c9dfb482a63fc83edf
soltanoff <piccadillable@gmail.com>
2018-02-05 19:39:14 +0300

    create Constants.py

---
+++ Utils/Constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from PyQt4 import QtCore
+
+# DATE
+CURRENT_DATE = QtCore.QDate.currentDate(QtCore.QDate())
+CURRENT_DATETIME = QtCore.QDateTime.currentDateTime(QtCore.QDateTime())
+DATE_LEFT_INF = QtCore.QDate(2000, 1, 1)
+DATE_RIGHT_INF = QtCore.QDate(2200, 1, 1)
+DATETIME_LEFT_INF = QtCore.QDateTime(DATE_LEFT_INF)
+DATETIME_RIGHT_INF = QtCore.QDateTime(DATE_RIGHT_INF)
