f992159bd1a02467e2c9d03a0040e91465ff0222
soltanoff <piccadillable@gmail.com>
2018-02-28 11:35:10 +0300

    added barcode thread implementation

---
+++ BarCodeReader/BarCodeReaderThread.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from PyQt4 import QtCore, QtGui
+
+from BarCodeReader.BarCodeWorker import CBarCodeWorker
+
+
+class CBarCodeReaderThread(QtCore.QThread):
+    omsMode = 'oms'
+    actionMode = 'action'
+    defaultMode = 'default'
+
+    availableModes = {
+        omsMode: CBarCodeWorker.readOMSBarCode,
+        actionMode: CBarCodeWorker.readActionBarCode,
+        defaultMode: CBarCodeWorker.defaultRead,
+    }
+
+    def __init__(self, port=u'COM3', mode='oms'):
+        QtCore.QThread.__init__(self)
+        self.barCodeReader = CBarCodeWorker(port)
+        self.barCodeReader.moveToThread(self)
+        self.busyByEvent = False
+        self.mode = mode
+
+    def disable(self):
+        self.barCodeReader.close = True
+        try:
+            QtGui.qApp.barcodeSerialPort.close()
+        except:
+            pass
+
+    def setFunc(self, mode):
+        self.mode = mode
+        self.barCodeReader.func = self.availableModes.get(mode, self.availableModes[self.defaultMode])
+
+    def run(self):
+        self.setFunc(self.mode)
+        self.barCodeReader.readData()
