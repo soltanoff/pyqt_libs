cc226b15e92b217de88504daee426309ffa92fb6
soltanoff <piccadillable@gmail.com>
2018-03-29 20:00:29 +0300

    create `compareCallStack`

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -36,6 +36,63 @@ def timer(func):
     return wrapper
 
 
+def compareCallStack(leftStack, rightStack, excludeLastCallFunctionName=None):
+    u"""
+    Сравнивает два стека
+    :return: (common_elements, parent_flag)
+        где common_elements - это количество первых одинаковых элементов обоих стеков
+        parent_flag - это флаг наследования, принимающий значение
+                      1, если функция левого стека является "родительской" для функции правого,
+                      0, если стеки относятся к несвязанным функциям,
+                      -1, если функция правого стека является "родительской" для функции левого
+    """
+    fullEqualityNumber = 0  # количество первых полностью схожих элементов стеков
+    isInherit = 0
+
+    if not (isinstance(leftStack, (list, tuple)) and isinstance(rightStack, (list, tuple))):
+        return (fullEqualityNumber, isInherit)
+
+    if not (leftStack and rightStack):
+        return (fullEqualityNumber, isInherit)
+
+    fileNameIdx = 0
+    lineNumberIdx = 1
+    callerIdx = 2
+    calleesIdx = 3
+
+    leftStackLength = len(leftStack)
+    rightStackLength = len(rightStack)
+
+    inheritanceDirection = 1
+    if rightStackLength < leftStackLength:
+        leftStackLength, rightStackLength = rightStackLength, leftStackLength
+        leftStack, rightStack = rightStack, leftStack
+        inheritanceDirection = -1
+
+    # В сборке имя вызываемой функции всегда NoneType
+    #    if (excludeLastCallFunctionName
+    #        and leftStack
+    #        and excludeLastCallFunctionName in leftStack[-1][calleesIdx]):
+    if excludeLastCallFunctionName:
+        leftStackLength -= 1
+
+    for stackIdx in xrange(leftStackLength):
+        isFileNameEquality = leftStack[stackIdx][fileNameIdx] == rightStack[stackIdx][fileNameIdx]
+        if (isFileNameEquality
+                and leftStack[stackIdx][lineNumberIdx] == rightStack[stackIdx][lineNumberIdx]
+                and leftStack[stackIdx][callerIdx] == rightStack[stackIdx][callerIdx]
+                and leftStack[stackIdx][calleesIdx] == rightStack[stackIdx][calleesIdx]):
+            fullEqualityNumber += 1
+        else:
+            break
+
+    isInherit = inheritanceDirection * (1 if fullEqualityNumber in [leftStackLength,  # та же функция
+                                                                    leftStackLength - 1]  # вложенная функция\
+                                        else 0)
+
+    return (fullEqualityNumber, isInherit)
+
+
 if __name__ == '__main__':
     import time
 
